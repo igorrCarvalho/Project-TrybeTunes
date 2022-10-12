7846f7c547de2cfc58ed7d4f5cf2b2e09b4ea9ae
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-12 18:18:13 -0300

    Cria exibição do perfil (Requisito 13)

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: {},
+      loading: false,
+    };
+
+    this.btnProfileClick = this.btnProfileClick.bind(this);
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const user = await getUser();
+    this.setState({ loading: false });
+    this.setState({ userData: user });
+  }
+
+  btnProfileClick() {
+    const { history } = this.props;
+    history.push('/profile/edit');
+  }
+
   render() {
+    const { loading, userData } = this.state;
+    const { image } = userData;
     return (
       <>
         <Header />
-        <div data-testid="page-profile" />
+        <div data-testid="page-profile">
+          { loading ? <Loading />
+            : (
+              <>
+                <div>
+                  <img
+                    data-testid="profile-image"
+                    src={ image }
+                    alt="Nenhuma foto salva"
+                  />
+                </div>
+                <div>
+                  <button
+                    onClick={ this.btnProfileClick }
+                    type="button"
+                  >
+                    Editar perfil
+                  </button>
+                </div>
+                <div>
+                  <h3>Nome</h3>
+                  <p>{ userData.name }</p>
+                </div>
+                <div>
+                  <h3>E-mail</h3>
+                  { userData.email === '' ? <p>Nenhum E-mail informado</p>
+                    : (
+                      <p>{ userData.email }</p>
+                    )}
+                </div>
+                <div>
+                  <h3>Descrição</h3>
+                  { userData.description === '' ? <p>Nenhuma descrição informada</p>
+                    : (
+                      <p>{ userData.description }</p>
+                    )}
+                </div>
+              </>
+            )}
+        </div>
       </>
     );
   }
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
